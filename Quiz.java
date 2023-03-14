28fb3163592fa8f0891a6ab7a0fa1973a26fac47
Ultragum <89540611+Ultragum@users.noreply.github.com>
2023-03-14 14:28:47 -0400

    Added more to the Question and Quiz classes

--- Quiz.java
+++ Quiz.java
@@ -33,4 +33,39 @@ public class Quiz {
      public String getTitle() {
         return title;
      }
+
+     public double getGrade() {
+        for (int i = 0; questions.size() > i; i++) {
+            if (!questions.get(i).hasUserAnswer()) {
+                System.out.println("You can't submit the quiz since you haven't answered all the questions");
+                return -1;
+            }
+        }
+
+        int correct = 0;
+
+        for (int i = 0; questions.size() > i; i++) {
+            if (questions.get(i).isCorrect()) {
+                correct++;
+            }
+        }
+
+        return correct/questions.size();
+     }
+
+     public void removeQuestion(String question) {
+        if (questions.size() == 0) {
+            System.out.println("There are no quizzes to remove");
+            return;
+        }
+        
+        for (int i = 0; questions.size() > i; i++) {
+            if (questions.get(i).getQuestion().equals(question)) {
+                questions.remove(i);
+                return;
+            }
+        }
+
+        System.out.println("That question could not be found");
+     }
 }

5cfa5a906962b0f82e24f42d47288e069ab5a3cd
raej15 <jonessara141@gmail.com>
2023-03-14 14:22:03 -0400

    user class stub done

---
+++ User.java
@@ -0,0 +1,50 @@
+/*
+ * Written by Anonmyous Pandas 
+ */
+
+ import java.util.UUID;
+
+ public class User {
+    private UUID UUID;
+    protected String username;
+    protected String password;
+    protected String email;
+    protected String firstName;
+    protected String lastName;
+
+    public User() {
+
+    }
+
+    public String toString() {
+        return "yay";
+    }
+
+    public String getUserName() {
+        return this.username;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    public int getUserType() {
+        return 1;
+    }
+
+    public boolean checkPassword(String password) {
+        return true;
+    }
+ }

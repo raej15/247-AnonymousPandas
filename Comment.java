cfdf5c3e6035c6c47d7011534fd85e0a2aff35f6
raej15 <jonessara141@gmail.com>
2023-03-14 15:54:01 -0400

    comment class stubbed

---
+++ Comment.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+
+/*
+ * Written by Anonymous Pandas
+ */
+
+
+public class Comment {
+    private String comment;
+    private Student user;
+    private ArrayList<Comment> comments;
+
+    public void Comment(Module module) {
+
+    }
+    
+    public void addComment(String comment) {
+
+    }
+
+    public void removeComment() {
+
+    }
+
+    public String getComment() {
+        return this.comment;
+    }
+
+
+}

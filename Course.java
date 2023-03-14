793ea14b5ab62ab0545b8ba74707f7572c446541
Ultragum <89540611+Ultragum@users.noreply.github.com>
2023-03-14 16:37:17 -0400

    Added comments to my parts

--- Course.java
+++ Course.java
@@ -2,8 +2,11 @@
  * Written by Anonymous Pandas
  */
 
- import java.util.ArrayList;
+import java.util.ArrayList;
 
+/**
+ * A course, which contains an ArrayList of modules, a name, a description, what language it's for, and if it's been completed
+ */
 public class Course {
     private ArrayList<Module> modules;
     private String courseName;
@@ -11,6 +14,12 @@ public class Course {
     private Language language;
     private boolean completed;
 
+    /**
+     * Creates a new course
+     * @param courseName The name of the course
+     * @param description What the course teachers the user
+     * @param language Which language the course is for
+     */
     Course(String courseName, String description, Language language) {
         modules = new ArrayList<Module>();
         this.courseName = courseName;
@@ -19,38 +28,75 @@ public class Course {
         completed = false;
     }
 
+    /**
+     * Adds a new module to the course
+     * @param moduleName The name of the new module
+     */
     public void addModule(String moduleName) {
         modules.add(new Module(moduleName));
     }
 
+    /**
+     * Updates the course name to something new
+     * @param courseName The new course name
+     */
     public void updateCourseName(String courseName) {
         this.courseName = courseName;
     }
 
+    /**
+     * Updates the course description to something new
+     * @param description The new description
+     */
     public void updateDescription(String description) {
         this.description = description;
     }
 
+    /**
+     * Updates the language to a new language
+     * @param language The new language
+     */
     public void updateLanguage(Language language) {
         this.language = language;
     }
 
+    /**
+     * Gets the name of the course
+     * @return courseName
+     */
     public String getCourseName() {
         return courseName;
     }
 
+    /**
+     * Gets the description of the course
+     * @return description
+     */
     public String getDescription() {
         return description;
     }
 
+    /**
+     * Returns the language that the course is for
+     * @return language
+     */
     public Language getLanguage() {
         return language;
     }
     
+    /**
+     * Returns the state of the completed variable
+     * @return True if the course has been finished, false if it has not
+     */
     public boolean getCompleted() {
         return completed;
     }
-
+    
+    /**
+     * Returns a module based on the inputted name, if it exists
+     * @param moduleName The module you want to get
+     * @return A module object
+     */
     public Module getModule(String moduleName) {
         if (modules.size() == 0) {
             System.out.println("There are no modules to get");
@@ -67,6 +113,10 @@ public class Course {
         return null;
     }
 
+    /**
+     * Removes a module via a name, if it exists
+     * @param moduleName The module to remove
+     */
     public void removeModule(String moduleName) {
         if (modules.size() == 0) {
             System.out.println("There are no modules to remove");
@@ -83,6 +133,9 @@ public class Course {
         System.out.println("That module could not be found");
     }
 
+    /**
+     * Changes the state of the completed variable
+     */
     public void toggleCompleted() {
         completed = !completed;
     }
--- Question.java
+++ Question.java
@@ -13,6 +13,10 @@ public class Question {
     private int correctAnswer;
     private int userAnswer;
 
+    /**
+     * Creates a new question
+     * @param question The question which the user must answer
+     */
     Question(String question) {
         answers = new ArrayList<String>();
         this.question = question;
@@ -20,30 +24,57 @@ public class Question {
         userAnswer = -1;
     }
 
+    /**
+     * Adds a new answer to the answer list
+     * @param answer The possible answer to the question
+     */
     public void addAnswer(String answer) {
         answers.add(answer);
     }
 
+    /**
+     * Sets an answer as correct
+     * @param answer The correct answer's number
+     */
     public void setCorrectAnswer(int answer) {
         correctAnswer = answer;
     }
-      
+    
+    /**
+     * Stores the user's answer
+     * @param answer The user's answer
+     */
     public void setUserAnswer(int answer) {
         userAnswer = answer;
     }
 
+    /**
+     * Gets the correct answer
+     * @return The correct answer
+     */
     public int getCorrectAnswer() {
         return correctAnswer;
     }
 
+    /**
+     * Gets the user's answer
+     * @return The user's answer
+     */
     public int getUserAnswer() {
         return userAnswer;
     }
 
+    /**
+     * Returns the question being asked
+     * @return The question
+     */
     public String getQuestion() {
         return question;
     }
 
+    /**
+     * Prints out all of the answers
+     */
     public void getAnswers() {
         if (answers.size() == 0) {
             System.out.println("There are no answers to return");
@@ -55,16 +86,22 @@ public class Question {
         }
     }
 
-      public boolean hasUserAnswer() {
+    /**
+     * Checks if the user has answered this question
+     * @return True if the user has, false if not
+     */
+    public boolean hasUserAnswer() {
         if (userAnswer == -1) {
             return false;
         } else {
             return true;
         }
-      }
-
-      
+    }
     
+    /**
+     * Removes an answer from the answer list
+     * @param answer The answer being removed
+     */
     public void removeAnswer(String answer) {
         if (answers.size() == 0) {
             System.out.println("There are no answers to remove");
@@ -81,6 +118,10 @@ public class Question {
         System.out.println("That answer could not be found");
     }
 
+    /**
+     * Checks if the user's answer is correct
+     * @return True if the user is correct, false if not
+     */
     public boolean isCorrect() {
         if (correctAnswer == userAnswer) {
             return true;

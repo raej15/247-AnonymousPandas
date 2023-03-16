02884086518c44a2350176f6c790aafbdb6dddcc
raej15 <jonessara141@gmail.com>
2023-03-16 14:00:22 -0400

    courselist progress

--- CourseList.java
+++ CourseList.java
@@ -7,10 +7,32 @@ import java.util.ArrayList;
 
 public class CourseList {
 
-    private CourseList courselist;
-    private ArrayList<Course> courses;
+    private static CourseList courselist;
+    public static ArrayList<Course> courses;
 
     CourseList() {
 
+    }
+    public static CourseList getInstance(){
+        if (courselist == null) {
+			courselist = new CourseList();
+		}
+        return courselist;
+    }
+
+    public static void setCourseList(ArrayList <Course> courses){
+        CourseList.courses = courses;
+    }
+
+    public Course getCourse(String courseName){
+        return null;
+    }
+
+    public void removeCourse(String username){
+       
+    }
+
+    public ArrayList<Course> getUsers() {
+        return courses;
     }
 }

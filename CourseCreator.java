537ced19c716662e305dadd36da418e30d0116de
Kelly Finnegan <kellfin9946@gmail.com>
2023-03-18 12:21:03 -0400

    add gettype to CC and student

--- CourseCreator.java
+++ CourseCreator.java
@@ -7,12 +7,22 @@ import java.util.UUID;
 
 public class CourseCreator extends User{
 
-    public String type;
+    public int type;
     public UUID uuid;
 
     public CourseCreator(UUID uuid, String username, String password, String email, String firstName, String lastName){
         super(username, password, email, firstName, lastName);
         this.uuid = uuid;
-        this.type = "Creator";
+        this.type = 2;
+    }
+
+    public CourseCreator(String username, String password, String email, String firstName, String lastName){
+        super(username, password, email, firstName, lastName);
+        this.uuid = UUID.randomUUID();
+        this.type = 2;
+    }
+
+    public int getType(){
+        return type;
     }
 }
--- Student.java
+++ Student.java
@@ -8,18 +8,22 @@ import java.util.UUID;
 
 public class Student extends User{
 
-    public String type;
+    public int type;
     public UUID uuid;
 
     public Student(UUID uuid, String username, String password, String email, String firstName, String lastName){
         super(username, password, email, firstName, lastName);
         this.uuid = uuid;
-        this.type = "Student";
+        this.type = 1;
     }
 
     public Student(String username, String password, String email, String firstName, String lastName){
         super(username, password, email, firstName, lastName);
         this.uuid = UUID.randomUUID();
-        this.type = "Student";
+        this.type = 1;
+    }
+
+    public int getType(){
+        return type;
     }
 }

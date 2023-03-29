90ac13b8f471da747820bbceba7d872f26f02c87
Kelly Finnegan <kellfin9946@gmail.com>
2023-03-29 15:15:54 -0400

    removed unused methods

--- FinalCertification.java
+++ FinalCertification.java
@@ -1,4 +1,3 @@
-import java.util.Scanner;
 
 /*
  * Written by Anonmyous Pandas 
@@ -26,30 +25,6 @@ import java.util.Scanner;
 
     public void setQuiz(Quiz quiz){
         this.quiz = quiz;
-    }
-
-    public void makeCert(){
-        Scanner sc = new Scanner(System.in);
-        Quiz newQuiz = new Quiz();
-        String continueQuiz = "Y";
-        while(continueQuiz.equals("Y")){
-            if (newQuiz.getQuestions().size() == 0){
-                newQuiz.newQuestion();
-                continue;
-            } else {
-                System.out.println("Would you like to add a question? (Y/N)");
-                continueQuiz = sc.nextLine();
-            }
-            if (continueQuiz.equals("N")) {
-                return;
-            } else if (continueQuiz.equals("Y")) {
-                newQuiz.newQuestion();
-            } else {
-                System.out.println("Invalid input");
-                continueQuiz = "Y";
-                continue;
-            }
-        }
     }
 
  }

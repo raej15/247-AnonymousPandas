217909709d6f4f2f67e7c89767aa8818760c88e4
Kelly Finnegan <kellfin9946@gmail.com>
2023-04-06 14:11:40 -0400

    DL testing

---
+++ DataLoaderTest.java
@@ -0,0 +1,48 @@
+import static org.junit.jupiter.api.Assertions.*;
+import java.util.ArrayList;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+//portias test
+public class DataLoaderTest {
+
+    private ArrayList<User> users = DataLoader.loadUsers();
+    private ArrayList<Course> courses = DataLoader.loadCourses();
+	
+	@BeforeEach
+	public void setup() {
+		users.clear();
+		users.add(new Student("Kelly", "Finnegan", "finnegak@email.sc.edu", "finnegak", "kfinn999!"));
+		//users.add(new CourseCreator("Rae"));
+		DataWriter.saveUsers();
+	}
+	
+	@AfterEach
+	public void tearDown() {
+		Users.getInstance().getUsers().clear();
+		DataWriter.saveUsers();
+	}
+	
+	
+	@Test
+	void testGetUsersSize() {
+		userList = DataLoader.getUsers();
+		assertEquals(2, userList.size());
+	}
+
+	@Test
+	void testGetUsersSizeZero() {
+		Users.getInstance().getUsers().clear();
+		DataWriter.saveUsers();
+		assertEquals(0, userList.size());
+	}
+	
+	@Test
+	void testGetUserFirstUserName() {
+		userList = DataLoader.getUsers();
+		assertEquals("asmith", userList.get(0).getUserName());
+	}
+
+    
+}

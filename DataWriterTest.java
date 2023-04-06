036c4d47152219701b61f03710b6d4295ab67890
Klutzgurl477 <yppatel@email.sc.edu>
2023-04-06 14:19:40 -0400

    testDataWriter

--- DataWriterTest.java
+++ DataWriterTest.java
@@ -1,12 +1,28 @@
 import static org.junit.jupiter.api.Assertions.*;
 import java.util.ArrayList;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class DataWriterTest {
     private UserList users = UserList.getInstance();
-    private ArrayList<User> userList = 
-    @Test
-    public void testing(){
-        assertTrue(true);
+    private ArrayList<User> userList = users.getUsers();
+
+    @BeforeEach
+	public void setup() {
+		UserList.getInstance().getUsers().clear();
+		DataWriter.saveUsers();
+	}
+	
+	@AfterEach
+	public void tearDown() {
+		UserList.getInstance().getUsers().clear();
+		DataWriter.saveUsers();
+	}
+
+    public void testWritingOneUser(){
+        userList.add(new User("Trey","White","twhite@gmail.com","trey12","treyWhite12"));
+        DataWriter.saveUsers();
     }
+
 }

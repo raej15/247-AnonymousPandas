3901a984f47389f99f36e2d1469df7e79c1ec2f1
Kennedyhhouston <89541449+Kennedyhhouston@users.noreply.github.com>
2023-04-06 13:39:43 -0400

    class

---
+++ UserListTest.java
@@ -0,0 +1,10 @@
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.Test;
+
+public class UserListTest{
+    @Test
+    public void testing(){
+        assertTrue(true);
+    }
+}

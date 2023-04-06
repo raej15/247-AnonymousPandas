8013d85deb6a1fe19239a74eeac022fbcd1ac2eb
raej15 <jonessara141@gmail.com>
2023-04-06 13:33:18 -0400

    test working courselist

---
+++ CourseListTest.java
@@ -0,0 +1,11 @@
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.Test;
+
+public class CourseListTest {
+    @Test
+    public void testing() {
+        assertTrue(true);
+    }
+    
+}

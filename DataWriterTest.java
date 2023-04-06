cbbf5807161e51050e92d28ad3a67f9e66b3fa19
Klutzgurl477 <yppatel@email.sc.edu>
2023-04-06 13:35:43 -0400

    setting datawritertest

---
+++ DataWriterTest.java
@@ -0,0 +1,15 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+
+import org.junit.jupiter.api.Test;
+
+class DataWriterTest {
+    private UserList users = UserList.getInstance();
+    private ArrayList<User> userList = users.getUsers();
+
+    @Test
+    public void testing(){
+        assertTrue(true);
+    }
+}

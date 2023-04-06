54fc00c8ac9b32560071d096c78044361af4faf0
Klutzgurl477 <yppatel@email.sc.edu>
2023-04-06 13:39:23 -0400

    test

--- DataWriterTest.java
+++ DataWriterTest.java
@@ -1,13 +1,10 @@
 import static org.junit.jupiter.api.Assertions.*;
-
 import java.util.ArrayList;
-
 import org.junit.jupiter.api.Test;
 
 class DataWriterTest {
     private UserList users = UserList.getInstance();
-    private ArrayList<User> userList = users.getUsers();
-
+    private ArrayList<User> userList = 
     @Test
     public void testing(){
         assertTrue(true);

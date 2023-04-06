8317a93be4cf8a74084c91724c63531b4ab00a4a
Kelly Finnegan <kellfin9946@gmail.com>
2023-04-06 14:38:45 -0400

    UST

--- UserListTest.java
+++ UserListTest.java
@@ -7,14 +7,9 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class UserListTest{
-    private UserList userList = UserList.getInstance();
-    private ArrayList<User> users = userList.getUsers();
 
-    @BeforeEach
-    @Test
-    public void setUp() {
-       // users.clear();
-        userList.add(new User("Trey", "White", "twhite@gmail.com", "trey12", "treyWhite12"));
-        DataWriter.saveUsers();
-    }
+@Test
+   public void testing(){
+    assertTrue(true);
+   }
 }

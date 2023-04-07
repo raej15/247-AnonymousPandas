a6c7d593c96a06db97a0420b447af80a5ef70f10
Kelly Finnegan <kellfin9946@gmail.com>
2023-04-07 12:29:33 -0400

    DL tests

--- DataLoaderTest.java
+++ DataLoaderTest.java
@@ -7,10 +7,11 @@ import org.junit.jupiter.api.Test;
 public class DataLoaderTest {
 
 	private static LMSFacade facade = new LMSFacade();
+	ArrayList <User> users = DataLoader.loadUsers();
+	private ArrayList<Course> courses = DataLoader.loadCourses();
 	
 	@Test
 	public void dataLoaderTest(){
-		ArrayList <User> users = DataLoader.loadUsers();
 		assertEquals(9, users.size());
 	}
 
@@ -24,70 +25,96 @@ public class DataLoaderTest {
 
 	@Test
 	public void zeroUsers(){
-		DataLoader.loadUsers(); // users.json empty
 		assertEquals(0, facade.getUserList().getUsers().size());
 	}
 
 	@Test
 	public void getUsernameTest(){
-		ArrayList <User> users = DataLoader.loadUsers();
 		assertEquals("trey12", users.get(0).getUserName());
 	}
 
 	@Test
 	public void getPasswordTest(){
-		ArrayList <User> users = DataLoader.loadUsers();
 		assertEquals("treyWhite12", users.get(0).getPassword());
 	}
 
 	@Test
 	public void getFirstNameTest(){
-		ArrayList <User> users = DataLoader.loadUsers();
 		assertEquals("Trey", users.get(0).getFirstName());
 	}
 
 	@Test
 	public void getLastNameTest(){
-		ArrayList <User> users = DataLoader.loadUsers();
 		assertEquals("White", users.get(0).getLastName());
 	}
 
 	@Test
 	public void getTypeTest(){
-		ArrayList <User> users = DataLoader.loadUsers();
 		assertEquals(1, users.get(0).getType());
 	}
 
 	@Test
 	public void getEmailTest(){
-		ArrayList <User> users = DataLoader.loadUsers();
 		assertEquals("twhite@gmail.com", users.get(0).getEmail());
 	}
 
 	@Test
-	public void getCertifications0(){
-		ArrayList <User> users = DataLoader.loadUsers();
+	public void getCertifications0Test(){
 		assertEquals(0, ((Student) users.get(0)).getCertifications().size());
 	}
 
 	@Test
-	public void getCertifications1(){
-		ArrayList <User> users = DataLoader.loadUsers();
+	public void getCertifications1Test(){
 		assertEquals(1, ((Student) users.get(8)).getCertifications().size());
 	}
 
 	@Test
-	public void getUserID(){
-		ArrayList <User> users = DataLoader.loadUsers();
+	public void getUserIDTest(){
 		assertEquals("87410238-bbc8-4bd0-813b-9c04c25bc8e1", users.get(0).getUUID().toString());
 	}
 
 	@Test
-	public void getCourseListSize(){
-		ArrayList<Course> courses = DataLoader.loadCourses();
+	public void getCourseListSizeTest(){
 		assertEquals(2, courses.size());
 	}
 
-	
+	@Test
+	public void getCourseNameTest(){
+		assertEquals("Introduction to JavaScript", courses.get(0).getCourseName());
+	}
+
+	@Test
+	public void getCourseDescriptionTest(){
+		assertEquals("JavaScript is a scripting language for creating dynamic web page content.", courses.get(0).getDescription());
+	}
+
+	@Test
+	public void getCourseLanguageTest(){
+		assertEquals("JavaScript", courses.get(0).getLanguage().toString());
+	}
+
+	@Test
+	public void getCourseCommentsCommentTest(){
+		String comment = courses.get(0).getCourseComments().get(0).getComment();
+		assertEquals("Who uses JavaScript?", comment);
+	}
+
+	@Test
+	public void getCourseCommentsUserTest(){
+		String commenter = courses.get(0).getCourseComments().get(0).getCommenter().toString();
+		assertEquals("1f9f3f3c-fead-4c41-b2ac-98aa2fd9ad58",commenter);
+	}
+
+	 @Test
+	 public void getCourseCommentsNestedCommentTest(){
+		String comment = courses.get(0).getCourseComments().get(0).getComments().get(0).getComment();
+		assertEquals("Front end developers use JavaScript to add behavior to webpages.", comment);
+	 }
+
+	 @Test
+	 public void getCourseCommentsNestedCommenterTest(){
+		String commenter = courses.get(0).getCourseComments().get(0).getComments().get(0).getCommenter().toString();
+		assertEquals("87410238-bbc8-4bd0-813b-9c04c25bc8e1",commenter);
+	 }
     
 }

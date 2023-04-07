6568d85e958fd74808011eba88b7c786690c8e70
Kelly Finnegan <kellfin9946@gmail.com>
2023-04-07 11:07:04 -0400

    DL tests

--- DataLoaderTest.java
+++ DataLoaderTest.java
@@ -24,8 +24,62 @@ public class DataLoaderTest {
 
 	@Test
 	public void zeroUsers(){
-		DataLoader.loadUsers();
+		DataLoader.loadUsers(); // users.json empty
 		assertEquals(0, facade.getUserList().getUsers().size());
+	}
+
+	@Test
+	public void getUsernameTest(){
+		ArrayList <User> users = DataLoader.loadUsers();
+		assertEquals("trey12", users.get(0).getUserName());
+	}
+
+	@Test
+	public void getPasswordTest(){
+		ArrayList <User> users = DataLoader.loadUsers();
+		assertEquals("treyWhite12", users.get(0).getPassword());
+	}
+
+	@Test
+	public void getFirstNameTest(){
+		ArrayList <User> users = DataLoader.loadUsers();
+		assertEquals("Trey", users.get(0).getFirstName());
+	}
+
+	@Test
+	public void getLastNameTest(){
+		ArrayList <User> users = DataLoader.loadUsers();
+		assertEquals("White", users.get(0).getLastName());
+	}
+
+	@Test
+	public void getTypeTest(){
+		ArrayList <User> users = DataLoader.loadUsers();
+		assertEquals(1, users.get(0).getType());
+	}
+
+	@Test
+	public void getEmailTest(){
+		ArrayList <User> users = DataLoader.loadUsers();
+		assertEquals("twhite@gmail.com", users.get(0).getEmail());
+	}
+
+	@Test
+	public void getCertifications0(){
+		ArrayList <User> users = DataLoader.loadUsers();
+		assertEquals(0, ((Student) users.get(0)).getCertifications().size());
+	}
+
+	@Test
+	public void getCertifications1(){
+		ArrayList <User> users = DataLoader.loadUsers();
+		assertEquals(1, ((Student) users.get(8)).getCertifications().size());
+	}
+
+	@Test
+	public void getUserID(){
+		ArrayList <User> users = DataLoader.loadUsers();
+		assertEquals("87410238-bbc8-4bd0-813b-9c04c25bc8e1", users.get(0).getUUID().toString());
 	}
     
 }

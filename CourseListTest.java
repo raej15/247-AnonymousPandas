c8049b7140fc04da738e04872536cbcb3a6a8786
raej15 <jonessara141@gmail.com>
2023-04-09 12:07:26 -0400

    added some tests

--- CourseListTest.java
+++ CourseListTest.java
@@ -7,24 +7,45 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-//portias test
+/**
+ * tested by Rae/Sara Jones
+ */
 public class CourseListTest {
-       //private CourseList courses = CourseList.getInstance();
-    //ArrayList<Course> courses = new ArrayList<Course>();
-    //private ArrayList<Course> courseList = courses.getCourses();
-    private ArrayList<Course> courses = DataLoader.loadCourses();
+
+	private static LMSFacade facade = new LMSFacade();
+	private ArrayList <User> users = DataLoader.loadUsers();
+	private ArrayList<Course> courses = DataLoader.loadCourses();
 
 
-	@BeforeEach
-	public void setup() {
-		courses.clear();
-		courses.add(new Course("JavaScript", "Teaches you how to code in javaScript!", Language.JavaScript, UUID.fromString("3541edda-cbd8-4018-b5c9-34088171d1ae")) );
-		DataWriter.saveCourses();
-	}
+	//@BeforeEach
+	//public void setup() {
+		//courses.clear();
+		//courses.add(new Course("JavaScript", "Teaches you how to code in javaScript!", Language.JavaScript, UUID.fromString("3541edda-cbd8-4018-b5c9-34088171d1ae")) );
+		//DataWriter.saveCourses();
+	//}
 
     @Test 
     public void testingCreateCourse() {
 
         assertTrue(true);
     }
+
+    @Test
+    void testingHasFirstCourse() {
+        boolean hasJavaScript = courses.get(0) != null;
+        assertTrue(hasJavaScript);
+    }
+    @Test 
+    public void testingGetInstance() {
+       boolean hasInstance = CourseList.getInstance() != null;
+       assertTrue(hasInstance);
+    }
+
+    @Test 
+    public void testCourseListSize() {
+		assertEquals(2, courses.size());
+
+    }
+
+
 }

e7522701762d93a6b30b3d3dc0b24c9ab8a8f39a
Kennedyhhouston <89541449+Kennedyhhouston@users.noreply.github.com>
2023-04-09 20:57:10 -0400

    testing

--- UserListTest.java
+++ UserListTest.java
@@ -10,79 +10,81 @@ import org.junit.jupiter.api.Test;
 
 public class UserListTest {
 
+    private UserList userList;
     private User user1;
     private User user2;
     private User user3;
- 
+
     @Before
     public void setUp() {
-        
-        user1 = new Student("Kennedy1", "last", "email", "username1", "password");
-        user2 = new Student("Kennedy2", "last", "email", "username2", "password");
-        user3 = new Student("Kennedy3", "last", "email", "username3", "password");
-        UserList.getInstance().getUsers().add(user1);
-        UserList.getInstance().getUsers().add(user2);
-        UserList.getInstance().getUsers().add(user3);
+        userList = UserList.getInstance();
+        user1 = new User("user1", "password1");
+        user2 = new User("user2", "password2");
+        user3 = new User("user3", "password3");
     }
- 
- 
+
     @Test
     public void testGetInstance() {
         UserList instance = UserList.getInstance();
         assertNotNull(instance);
     }
  
- 
     @Test
-    public void testGetUser() {
-        User foundUser = UserList.getInstance().getUser("username1");
-        assertEquals(user1, foundUser);
-        User notFoundUser = UserList.getInstance().getUser("username4");
-        assertNull(notFoundUser);
-    }
- 
- 
+   public void testGetUser() {
+       User foundUser = UserList.getInstance().getUser("username1");
+       assertEquals(user1, foundUser);
+       User notFoundUser = UserList.getInstance().getUser("username4");
+       assertNull(notFoundUser);
+   }
+
+
     @Test
     public void testHas() {
-        boolean hasUser1 = UserList.getInstance().has("username1");
-        //assertTrue(hasUser1); THIS ONE ISNT WORKING
-        boolean hasUser4 = UserList.getInstance().has("username4");
-        assertFalse(hasUser4);
+        userList.addUser(user1);
+        userList.addUser(user2);
+        userList.addUser(user3);
+
+        assertTrue(userList.has("user2"));
+        assertFalse(userList.has("user4"));
     }
- 
- 
+
     @Test
     public void testLogin() {
-        //boolean validLogin = UserList.getInstance().login(user1, "password");
-        //assertTrue(validLogin);
-        boolean invalidPassword = UserList.getInstance().login(user1, "wrongpassword");
-        assertFalse(invalidPassword);
-        //boolean nullUser = userList.login(null, "password1");
-        //assertFalse(nullUser);
+        userList.addUser(user1);
+        userList.addUser(user2);
+        userList.addUser(user3);
+
+        assertTrue(userList.login(user3, "password3"));
+        assertFalse(userList.login(user1, "wrongpassword"));
     }
- /*
+
     @Test
-    public void testGetUsers() {
+    public void testPrintUsers() {
+        userList.addUser(user1);
+        userList.addUser(user2);
+        userList.addUser(user3);
+
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+
+        userList.printUsers();
+        String expectedOutput = "User [userName=user1, password=password1]\n---------------\n" +
+                                "User [userName=user2, password=password2]\n---------------\n" +
+                                "User [userName=user3, password=password3]\n---------------\n";
+        assertEquals(expectedOutput, outContent.toString());
+    }
+
+    @Test
+    public void testAddUser() {
+        userList.addUser(user1);
+        userList.addUser(user2);
+        userList.addUser(user3);
+
         ArrayList<User> users = userList.getUsers();
         assertEquals(3, users.size());
         assertTrue(users.contains(user1));
         assertTrue(users.contains(user2));
         assertTrue(users.contains(user3));
     }
- 
- 
-    @Test
-    public void testAddUser() {
-        Student newUser = new Student("Kennedy", "last", "email", "username", "password");
-        userList.addUser(newUser);
-        assertTrue(userList.has("user4"));
-    }
- 
- 
-    @Test
-    public void testPrintUsers() {
-        userList.printUsers();
-    }
-    */
- }
- 
+
+}
